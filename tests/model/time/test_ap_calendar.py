6574c3ed467e6eedc836cc174e0b2ff1f97dbf81
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-05-09 12:21:03 -0400

    Update test_ap_calendar.py

--- tests/model/time/test_ap_calendar.py
+++ tests/model/time/test_ap_calendar.py
@@ -1,20 +1,25 @@
+""" Testing for accounting calendar operations """
 import datetime
 import unittest
 
-from model.time.ap_calendar import APCalendar, days_in_month, net_days
+from model.time import net_days
+from model.time.ap_calendar import APCalendar, DAYS_IN_MONTH
 
 
 class TestAPCalendar(unittest.TestCase):
+    """ Test the initialization and usage of APCalendar objects """
 
     def setUp(self) -> None:
         self.cal = APCalendar(year=2018)
         self.c_leap = APCalendar(year=2020)
 
     def test_init(self):
+        """ Initialization works """
         self.assertEqual(False, self.cal.is_leap)
         self.assertEqual(True, self.c_leap.is_leap)
 
     def test_start_date_monthly(self):
+        """ Start date of the month """
         for i in range(1, 13):
             date = self.cal.start_date(i)
             print(date)
@@ -24,6 +29,7 @@ class TestAPCalendar(unittest.TestCase):
             self.assertEqual('01', str(self.cal.start_date(i))[-2:])
 
     def test_start_date_monthly_leap(self):
+        """ Start date of the month in leap years """
         for i in range(1, 13):
             date = self.c_leap.start_date(i)
             print(date)
@@ -33,45 +39,51 @@ class TestAPCalendar(unittest.TestCase):
             self.assertEqual('01', str(self.c_leap.start_date(i))[-2:])
 
     def test_end_date_monthly(self):
+        """ Check end of the month dates in non-leap years"""
         for i in range(1, 13):
             date = self.cal.end_date(i)
             print(date)
-            self.assertEqual(str(days_in_month[i]), str(date)[-2:])
+            self.assertEqual(str(DAYS_IN_MONTH[i]), str(date)[-2:])
         self.cal.update_year(2029)
         for i in range(1, 13):
             date = self.cal.end_date(i)
             print(date)
-            self.assertEqual(str(days_in_month[i]), str(date)[-2:])
+            self.assertEqual(str(DAYS_IN_MONTH[i]), str(date)[-2:])
 
     def test_end_date_monthly_leap(self):
+        """ Check the end of the month dates in leap years """
         self.assertEqual(str(31), str(self.c_leap.end_date(1))[-2:])
         self.assertEqual(str(29), str(self.c_leap.end_date(2))[-2:])
         for i in range(3, 13):
             date = self.c_leap.end_date(i)
             print(date)
-            self.assertEqual(str(days_in_month[i]), str(date)[-2:])
+            self.assertEqual(str(DAYS_IN_MONTH[i]), str(date)[-2:])
         self.c_leap.update_year(2024)
         self.assertEqual(str(31), str(self.c_leap.end_date(1))[-2:])
         self.assertEqual(str(29), str(self.c_leap.end_date(2))[-2:])
         for i in range(3, 13):
             date = self.c_leap.end_date(i)
             print(date)
-            self.assertEqual(str(days_in_month[i]), str(date)[-2:])
+            self.assertEqual(str(DAYS_IN_MONTH[i]), str(date)[-2:])
 
     def test_net_days(self):
         """ Check net 30 days due date calculations """
-        self.assertEqual(datetime.date(2018, 1, 31),
-                         net_days(datetime.date(2018, 1, 1), 30)
-                         )
-        self.assertEqual(datetime.date(2018, 3, 2),
-                         net_days(datetime.date(2018, 1, 31), 30)
-                         )
-        self.assertEqual(datetime.date(2020, 3, 1),
-                         net_days(datetime.date(2020, 1, 31), 30)
-                         )
-        self.assertEqual(datetime.date(2020, 3, 21),
-                         net_days(datetime.date(2020, 2, 20), 30)
-                         )
+        self.assertEqual(
+            datetime.date(2018, 1, 31),
+            net_days(datetime.date(2018, 1, 1), 30)
+        )
+        self.assertEqual(
+            datetime.date(2018, 3, 2),
+            net_days(datetime.date(2018, 1, 31), 30)
+        )
+        self.assertEqual(
+            datetime.date(2020, 3, 1),
+            net_days(datetime.date(2020, 1, 31), 30)
+        )
+        self.assertEqual(
+            datetime.date(2020, 3, 21),
+            net_days(datetime.date(2020, 2, 20), 30)
+        )
 
 
 if __name__ == '__main__':
