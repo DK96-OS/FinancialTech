d874b57bfbb697372abae1235830f942427631af
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-05-09 22:59:24 -0400

    Delete data_exceptions.py

--- model/data/data_exceptions.py
+++
@@ -1,9 +0,0 @@
-
-class CurrencyException(Exception):
-    """ Exception for Currency comparison errors """
-    pass
-
-
-class DateOutOfBoundsException(Exception):
-    """ A date is outside of the required boundary values """
-    pass
