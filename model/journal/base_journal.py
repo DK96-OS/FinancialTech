e436bc8fee6fe0220a95e00400568374cb69732a
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-10-18 22:34:37 -0400

    Update base_journal.py
    * Modify inheritance

--- model/journal/base_journal.py
+++ model/journal/base_journal.py
@@ -1,20 +1,16 @@
 """ Basic Journal functionality """
-import datetime
 
+from model.journal.journal_attributes import JournalAttributes
 from model.journal.journal_entry import JournalEntry
-from model.time.accounting_period import AccountingPeriod
 
 
-class BaseJournal(AccountingPeriod):
-    """ A Journal for one accounting period """
+class BaseJournal:
+    """ The basic Journal functionality """
 
     def __init__(self,
-                 name: str,
-                 opening_date: datetime.date,
-                 closing_date: datetime.date,
+                 attributes: JournalAttributes,
                  ):
-        super().__init__(opening_date, closing_date)
-        self.name = name
+        self.attributes = attributes
         self._entries = []
 
     def insert_entry(self, event: JournalEntry) -> bool:
@@ -22,7 +18,7 @@ class BaseJournal(AccountingPeriod):
         Verifies that the Entry date is within the Accounting Period
         :return Whether the Entry belongs in this journal
         """
-        if self.contains_date(event.date):
+        if self.attributes.contains_date(event.date):
             self._entries.append(event)
             return True
         else:
