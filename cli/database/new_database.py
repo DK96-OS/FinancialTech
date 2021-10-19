17f792eac68dbf99b3aef6692658dfa502cfcfbb
DK96 <69859316+DK96-OS@users.noreply.github.com>
2021-10-18 22:29:36 -0400

    Create new_database.py

---
+++ cli/database/new_database.py
@@ -0,0 +1,14 @@
+""" Setup a new database """
+
+
+def determine_database_attributes():
+    """ Obtain the necessary database attributes """
+    cli_input = input("Database Name :")
+
+    cli_input = input("Single Accounting Period? (y or n) :")
+
+    cli_input = input("Length of Accounting Period (Month or Year) :")
+
+    cli_input = input("Single Journal? (y or n) :")
+
+    cli_input = input("Encryption? (y or n) :")
